f0390b8cc110cdbd575424aaa0be7beb2ddccfb7
Jude Asiedu <jude.asiedu99@gmail.com>
2024-03-13 23:17:55 +0000

    Updates to package and removed unwanted code,added new components,add more routes

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { SiteSettingsComponent } from './site-settings/site-settings.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    BlogComponent,
+    SiteSettingsComponent
   ],
   imports: [
     CommonModule,
