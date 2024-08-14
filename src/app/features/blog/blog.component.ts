f338e67441d4e7f2ae3eb88621489b7cb0e41e4e
Jude Asiedu <jude.asiedu99@gmail.com>
2024-08-14 10:58:12 +0000

    added no data state and redirected links properly

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent {
+export class BlogComponent  implements OnInit{
+
+  blogPost:any[]=[]
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 
 }
