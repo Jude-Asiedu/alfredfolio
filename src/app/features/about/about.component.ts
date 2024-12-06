b379f8ef85d4815d2f220baae5c27931b380cf9e
Jude Asiedu <jude.asiedu99@gmail.com>
2024-12-06 01:17:51 +0000

    Added updates to the about page with better text to be updated lated

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+
+  constructor(){
+    
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+
+  sendForm(){
+
+  }
 }
